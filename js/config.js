4ede498e112ba514d7fe6c460e1694b7bebd28b9
Zulayman <zulayman21@gmail.com>
2020-10-03 23:16:09 +0800

    updated config to add user pool creds

--- js/config.js
+++ js/config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: process.env.ENV === "production" ? USERPOOLID : process.env.USERPOOLID, // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: process.env.ENV === "production" ? USERPOOLCLIENTID : process.env.USERPOOLCLIENTID, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
